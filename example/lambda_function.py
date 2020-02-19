adc294f049b249a6fe75ffce0f6ec31d8f367c3e
vubon <vubon.roy@gmail.com>
2020-02-19 11:48:30 +0600

    github action test

--- example/lambda_function.py
+++ example/lambda_function.py
@@ -2,7 +2,7 @@ import json
 
 
 def lambda_handler(event, context):
-    print("All features test")
+    print("All new test")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
